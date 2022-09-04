1e4faca19b29d5267b0c824cffd180028517a2eb
Mojtabarezaei4 <mojtabarezaei2001@gmail.com>
2022-09-04 20:34:09 +0200

    the coloring in process

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -1,7 +1,8 @@
 ﻿
 List<string> scanner = Scanner("29535123p48723487597645723645");
-Console.WriteLine(Sum(scanner));
-//29535123p48723487597645723645
+Console.WriteLine("Sum: " + Sum(scanner));
+//2953512 3p48723487597645723645
+Coloring("29535123p48723487597645723645", scanner);
 
 static List<string> Scanner(string input)
 {
@@ -13,7 +14,6 @@ static List<string> Scanner(string input)
         if (char.IsDigit(input[i]))
         {
             result += input[i];
-
             for (int j = i + 1; j < input.Length; j++)
             {
                 if (!char.IsDigit(input[j]))
@@ -47,6 +47,19 @@ static List<string> Scanner(string input)
 
     return results;
 }
+
+static void Coloring(string theString, List<string> input)
+{
+    List<string> temp = new ;
+    for (int i = 0; i < input.Count; i++)
+    {
+        theString.Replace(input[i], "---");
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
 
 static long Sum(List<string> input)
 {
